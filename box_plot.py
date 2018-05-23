e352b54ce52bf45c86e3c676e7ff7cbd9c3e0b0f
oysteiae <oysteiae@stud.ntnu.no>
2018-05-23 20:53:12 +0200

    Started working on a way to plot error map

---
+++ box_plot.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+def read_scores(path_to_file, index):
+    scores = []
+
+    with open(path_to_file, 'r') as log:
+        log = csv.reader(log, delimiter='\t')
+        first = True
+
+        for row in log:
+            if(first):
+                first = False
+                continue
+            scores.append(float(row[index]))
+
+    return scores
+
+def box_plot(path_to_scores_cnn, path_to_scores_unet, path_to_scores_deepmedic, index):
+    bottom = 0.7
+    top = 1.0
+
+    cnn_scores = read_scores(path_to_scores_cnn, index)
+    unet_scores = read_scores(path_to_scores_unet, index)
+    deepmedic_scores = read_scores(path_to_scores_deepmedic, index)
+
+    data_to_plot = [cnn_scores, unet_scores, deepmedic_scores]
+
+    fig = plt.figure(1, figsize=(9, 6))
+    # Create an axes instance
+    ax = fig.add_subplot(111)
+    # Create the boxplot
+    bp = ax.boxplot(data_to_plot)
+
+    architectures = ["CNN", "U-Net", "DeepMedic"]
+    ax.set_ylim(bottom, top)
+    ax.set_xticklabels(architectures,
+                        rotation=45, fontsize=8)
+    plt.show()
+
+box_plot("C:\\Users\\oyste\\Documents\\Master\\Experiments\\CNNAll\\CNNAll_scores.tsv", "C:\\Users\\oyste\\Documents\\Master\\Experiments\\UnetAll\\UnetAll_scores.tsv", "C:\\Users\\oyste\\Documents\\Master\\deepmedicExperiments\\DeepMedicAll_scores.tsv", 1)
--- helper.py
+++ helper.py
@@ -1,3 +1,27 @@
+# Taken from https://github.com/GUR9000/Deep_MRI_brain_extraction
+def load_files(data_file_location):
+    data = []
+
+    startswith = None
+    endswith = None
+    contains = None
+    contains_not = None
+
+    for path in data_file_location:
+        gg = [(_join(path, f) if path != "." else f) for f in _listdir(path) if
+              _isfile(_join(path, f)) and (startswith == None or f.startswith(startswith)) and (
+              endswith == None or f.endswith(endswith)) and (contains == None or contains in f) and (
+              contains_not == None or (not (contains_not in f)))]
+        data.append(gg)
+
+    combined_list = []
+    # Sort the lists:
+    for i in range(len(data)):
+        elem = sorted(data[i])
+        combined_list = combined_list + elem
+
+    return combined_list
+
 def compute_scores(pred, label):
     assert pred.shape == label.shape, "Shape mismatch between prediction and label when calculating scores"
     shape = pred.shape
